b1561c4ae24d5754ed195e0db26580195eabf5e5
cinda87 <j.c.bietz@gmail.com>
2025-03-27 13:20:10 -0600

    chore: install Bootstrap

--- skyglance/src/App.jsx
+++ skyglance/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Footer from "./Components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      <h1>Weather App</h1>
-      <Footer />
-    </>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
