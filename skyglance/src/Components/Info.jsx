333337f7843c2f9dc0613dbef47651a1d7f48eba
cinda87 <j.c.bietz@gmail.com>
2025-04-02 21:26:50 -0600

    ui: improve Weather Details section and streamline WindCompass integration

--- skyglance/src/Components/Info.jsx
+++ skyglance/src/Components/Info.jsx
@@ -45,20 +45,21 @@ export default function WeatherInfo(props) {
       </div>
 
       <div className="box" style={{ gridArea: "box-3" }}>
-        <ul className="list-unstyled m-0">
+        <h5 className="text-center mb-2">Weather Details</h5>
+        <ul className="list-unstyled m-0 mx-auto">
           <li>
             Feels Like: {convertTemperature(props.data.feelsLike)}º
             {unit === "metric" ? "C" : "F"}
           </li>
+          <li>Humidity: {props.data.humidity}%</li>
           <li>
             Wind: {convertWind(props.data.wind)}{" "}
             {unit === "metric" ? "km/h" : "mph"}
           </li>
-
-          <li> Humidity: {props.data.humidity}%</li>
           <li>Pressure: {props.data.pressure} hPa</li>
         </ul>
       </div>
+
       <div className="box" style={{ gridArea: "box-4" }}>
         <Pressure pressure={props.data.pressure} />
       </div>
@@ -66,11 +67,7 @@ export default function WeatherInfo(props) {
         <Humidity humidity={props.data.humidity} />
       </div>
       <div className="box" style={{ gridArea: "box-6" }}>
-        <WindCompass
-          windSpeed={props.data.wind}
-          windDeg={props.data.windDeg}
-          unit={unit}
-        />
+        <WindCompass windDeg={props.data.windDeg} />
       </div>
     </div>
   );
