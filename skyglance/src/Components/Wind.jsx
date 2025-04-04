aafb79751c7a2d585a274c61ea4e71517b4cb252
cinda87 <j.c.bietz@gmail.com>
2025-04-03 20:05:17 -0600

    feat: add animated weather icon to wind compass and update title styling

--- skyglance/src/Components/Wind.jsx
+++ skyglance/src/Components/Wind.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../Style/Wind.css";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WindCompass({ windDeg }) {
   return (
     <div className="dial-container">
-      <div className="title">💨 Wind</div>
+      <div className="title">Wind</div>
       <div className="compass-container">
         <div className="compass">
           <div
@@ -12,6 +13,14 @@ export default function WindCompass({ windDeg }) {
             style={{ transform: `rotate(${windDeg}deg)` }}
           ></div>
           <div className="center-dot"></div>
+          <div className="icon-center">
+            <ReactAnimatedWeather
+              icon="WIND"
+              color="#2f3e4d"
+              size={60}
+              animate
+            />
+          </div>
           <div className="dir n">N</div>
           <div className="dir e">E</div>
           <div className="dir s">S</div>
